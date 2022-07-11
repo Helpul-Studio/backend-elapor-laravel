25227d0ea3c74e774ced742d2f6a26037a7ceb10
dika <mahardikastudioh@gmail.com>
2022-07-11 15:05:54 +0800

    adding admin template

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,14 @@
+<script src="{{ url('admin-template/assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ url('admin-template/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ url('admin-template/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+        <script src="{{url('admin-template/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{url('admin-template/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{url('admin-template/assets/libs/feather-icons/feather.min.js')}}"></script>
+        <!-- pace js -->
+        <script src="{{url('admin-template/assets/libs/pace-js/pace.min.js')}}"></script>
+
+        <script src="https://maps.google.com/maps/api/js?key=AIzaSyCtSAR45TFgZjOs4nBFFZnII-6mMHLfSYI"></script>
+
+        <script src="{{url('admin-template/assets/js/app.js')}}"></script>
+
+        <script src="{{url('admin-template/assets/js/ajax.js')}}"></script>
---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="{{ url('admin-template/assets/css/preloader.min.css') }}"
+            type="text/css" />
+
+        <!-- Bootstrap Css -->
+        <link href="{{ url('admin-template/assets/css/bootstrap.min.css') }}" id="bootstrap-style"
+            rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ url('admin-template/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
+        <!-- App Css-->
+        <link href="{{ url('admin-template/assets/css/app.min.css') }}" id="app-style" rel="stylesheet"
+            type="text/css" />
+
+        <!-- Custom Css -->
+        <link href="{{ url('admin-template/assets/css/custom.css') }}" rel="stylesheet" type="text/css" />
