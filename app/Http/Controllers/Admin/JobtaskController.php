119f7086e765b7021cfed3619db13d4a45db042b
Farhan Rivaldy <fariv.fariv12@gmail.com>
2022-08-02 19:45:44 +0800

    fix fe

---
+++ app/Http/Controllers/Admin/JobtaskController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JobtaskController extends Controller
+{
+    public function getAllUser()
+    {
+        $users = User::all();
+        return json_encode(['data' => $users]);
+    }
+
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);   
+        $user->occupation = $request->occupation;
+        $user->user_role = $request->user_role;
+        if ($request->hasFile('user_photo')) {
+            $validate = Validator::make($request->all(), [
+                'user_photo' => 'required|mimes:png,jpg,jpeg'
+            ]);
+            $user->user_photo = $request->file('user_photo')->store('user_photo', 'public');
+        }
+
+        $user->save();
+
+        return response()->json(['status' => true]);
+    }
+
+    public function edit($id)
+    {
+        $user = User::where('user_id', $id)->first();
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->occupation = $request->occupation;
+        $user->user_role = $request->user_role;
+        
+    if ($request->hasFile('user_photo')) {
+            $validate = Validator::make($request->all(), [
+                'user_photo' => 'required|mimes:png,jpg,jpeg'
+            ]);
+            if ($user->user_photo != null) {
+            Storage::disk('public')->delete($user->user_photo);
+            $user->user_photo = $request->file('user_photo')->store('user_photo', 'public');
+        }
+        $user->user_photo = $request->file('user_photo')->store('user_photo', 'public');
+    }
+
+        if($request->has('password')){
+            $user->password = Hash::make($request->password);   
+        }
+        $user->save();
+        return response()->json(['status' => true]);
+    }
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        try {
+            Storage::disk('public')->delete($user->user_photo);
+            $user->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Data successfully deleted'
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->errorInfo
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('getAllUser', [App\Http\Controllers\Admin\UserController::class, 'getAllUser'])->name('getAllUser');
 Route::get('getAllStructural', [App\Http\Controllers\Admin\StructuralController::class, 'getAllStructural'])->name('getAllStructural');
+Route::get('getAllJobtask', [App\Http\Controllers\Admin\StructuralController::class, 'getAllJobtask'])->name('getAllJobtask');
 
 
 Route::prefix('admin')->middleware('auth')->group(function(){
@@ -40,13 +41,20 @@ Route::prefix('admin')->middleware('auth')->group(function(){
         Route::delete('/user/delete-user/{id}', 'destroy')->name('user.destroy');
     });
 
-
-
     Route::controller(\App\Http\Controllers\Admin\StructuralController::class)->group(function(){
         Route::get('/structural', 'index')->name('structural.index');
         Route::post('/structural/add-structural', 'store')->name('structural.store');
         Route::get('/structural/get-structural/{id}', 'edit')->name('structural.edit');
         Route::put('/structural/update-structural/{id}', 'update')->name('structural.update');
         Route::delete('/structural/delete-structural/{id}', 'destroy')->name('structural.destroy');
+    });
+
+
+    Route::controller(\App\Http\Controllers\Admin\JobtaskController::class)->group(function(){
+        Route::get('/jobtask', 'index')->name('jobtask.index');
+        Route::post('/jobtask/add-jobtask', 'store')->name('jobtask.store');
+        Route::get('/jobtask/get-jobtask/{id}', 'edit')->name('jobtask.edit');
+        Route::put('/jobtask/update-jobtask/{id}', 'update')->name('jobtask.update');
+        Route::delete('/jobtask/delete-jobtask/{id}', 'destroy')->name('jobtask.destroy');
     });
 });
