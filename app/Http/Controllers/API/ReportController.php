066ebfdaa8528c4a4ffa040e3273875b8350c8a1
Farhan Rivaldy <fariv.fariv12@gmail.com>
2022-08-15 11:54:16 +0800

    fix upload report

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\JobtaskResult;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ReportController extends Controller
 {
@@ -61,7 +62,33 @@ class ReportController extends Controller
                     'report_task_id' => $randID.$user,
                 ]);
             }
-            return ResponseFormatter::success($data, 'Berhasil Upload Laporan', 200);
+
+            $report = new JobtaskResult();
+            $report->report_type = 'Isidentil';
+            $report->report_task_id = $randID.$user;
+            $report->subordinate = Auth::user()->user_id;
+            $report->location_latitude = $request->location_latitude;
+            $report->location_longitude = $request->location_longitude;
+
+            if ($request->hasFile('jobtask_documentation')) {
+                $validate = Validator::make($request->all(), [
+                    'jobtask_documentation' => 'required|mimes:png,jpg,jpeg'
+                ]);
+                $report->jobtask_documentation = $request->file('jobtask_documentation')->store('jobtask_documentation', 'public');
+            }
+
+            $report->report_about = $request->report_about;
+            $report->report_source_information = $request->report_source_information;
+            $report->report_date = $request->report_date; 
+            $report->report_place = $request->report_place;
+            $report->report_activities = $request->report_activities; 
+            $report->report_analysis = $request->report_analysis;
+            $report->report_prediction = $request->report_prediction;
+            $report->report_steps_taken = $request->report_steps_taken;
+            $report->report_recommendation = $request->report_recommendation;
+            $report->save();
+
+            return ResponseFormatter::success($report, 'Berhasil Upload Laporan', 200);
         }
 
     }
